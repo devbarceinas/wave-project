c7cc88b74e6f163af204e4dedc69fd5c6ea5ec60
devbarceinas <dev.mariobarceinas@gmail.com>
2024-08-31 19:39:22 -0600

    feat(badge): a badge component was created

---
+++ src/stories/Badge.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+// import { fn } from '@storybook/test';
+
+import { Badge } from './Badge';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/Badge',
+  component: Badge,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Success: Story = {
+  args: {
+    variant: 'success',
+    text: 'Enviado',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    variant: 'warning',
+    text: 'Pendiente',
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    variant: 'danger',
+    text: 'Cancelado',
+  },
+};
---
+++ src/stories/Badge.tsx
@@ -0,0 +1,20 @@
+import './badge.css';
+
+export interface BadgeProps {
+  variant: 'success' | 'warning' | 'danger';
+  text: String;
+};
+
+export const Badge = ({
+  variant,
+  text
+}: BadgeProps) => {
+
+  return (
+    <span className={
+      `storybook-badge storybook-badge--${variant}`
+    }>
+      {text}
+    </span>
+  );
+};
